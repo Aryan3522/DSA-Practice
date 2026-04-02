f755ff2a841b4c44cc4dbaa0c6547dd3b99164fd
Aryan <aryanhooda3522@gmail.com>
2026-04-02 16:47:16 +0530

    update main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import StarTriangle.Triangle;
 
 void main() {
     Square sq = new Square();
-//    sq.square(5);
+    sq.square(5);
 
     Triangle t = new Triangle();
-//    form a right angled triangle
+//    form a right-angled triangle
     t.triange(5);
-    System.out.println("-------------------------------------");
-//    form a reverse right angled triangle
+
+//    form a reverse right-angled triangle
     t.revTriangle(5);
 }
