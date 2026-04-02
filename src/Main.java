7cfbe72d1d40d93f811600b88e060859cc7a0a81
Aryan <aryanhooda3522@gmail.com>
2026-04-02 17:15:36 +0530

    feat(ques): add star, number triangle formation through various methods

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,16 @@ void main() {
     Triangle t = new Triangle();
 //    form a right-angled triangle
     t.triange(5);
-
 //    form a reverse right-angled triangle
     t.revTriangle(5);
+//    form a num triangle
+    t.NumTriangle(5);
+//    num triangle 2
+    t.NumTriangle2(5);
+//    reverse num triangle
+    t.revNumTriangle(5);
+//    reverse num triangle 2
+    t.revNumTriangle2(5);
+//    reverse num triangle 3
+    t.revNumTriangle3(5);
 }
--- src/StarTriangle/Triangle.java
+++ src/StarTriangle/Triangle.java
@@ -19,4 +19,49 @@ public class Triangle {
         }
         return -1;
     }
+    public int NumTriangle(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        return -1;
+    }
+    public int NumTriangle2(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        return -1;
+    }
+    public int revNumTriangle(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = n; j >= i; j--){
+                System.out.print((n - j + 1));
+            }
+            System.out.println();
+        }
+        return -1;
+    }
+    public int revNumTriangle2(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = n; j >= i; j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        return -1;
+    }
+    public int revNumTriangle3(int n){
+        for(int i = n; i > 0; i--){
+            for(int j = i; j > 0; j--){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        return -1;
+    }
 }
