4a65de6b53042905968f8fcc5484d8911226b7c9
Aryan <aryanhooda3522@gmail.com>
2026-04-03 14:38:43 +0530

    add new ques solutions

--- src/StarTriangle/Triangle.java
+++ src/StarTriangle/Triangle.java
@@ -1,64 +1,142 @@
 package StarTriangle;
 
 public class Triangle {
-    public int triange(int n){
-        for (int i = 0; i < n; i++){
-            for(int j = 0; j <= i; j++){
+    public int triange(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         return -1;
     }
-    public int revTriangle(int n){
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
+
+    public int revTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         return -1;
     }
-    public int NumTriangle(int n){
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
+
+    public int triangle2(int n) {
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars = i;
+            if (i > n) stars = 2 * n - i;
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        return -1;
+    }
+
+    public int NumTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
             System.out.println();
         }
         return -1;
     }
-    public int NumTriangle2(int n){
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
+
+    public int NumTriangle2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(i);
             }
             System.out.println();
         }
         return -1;
     }
-    public int revNumTriangle(int n){
-        for(int i = 1; i <= n; i++){
-            for(int j = n; j >= i; j--){
+
+    public int revNumTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
                 System.out.print((n - j + 1));
             }
             System.out.println();
         }
         return -1;
     }
-    public int revNumTriangle2(int n){
-        for(int i = 1; i <= n; i++){
-            for(int j = n; j >= i; j--){
+
+    public int revNumTriangle2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
                 System.out.print(j);
             }
             System.out.println();
         }
         return -1;
     }
-    public int revNumTriangle3(int n){
-        for(int i = n; i > 0; i--){
-            for(int j = i; j > 0; j--){
+
+    public int revNumTriangle3(int n) {
+        for (int i = n; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
                 System.out.print(i);
+            }
+            System.out.println();
+        }
+        return -1;
+    }
+
+    public int pyramid(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        return -1;
+    }
+
+    public int revPyramid(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        return -1;
+    }
+
+    public int diamond(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
             }
             System.out.println();
         }
