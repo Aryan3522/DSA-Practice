e8981c9f088c644f507897a3c69569604a1e0729
Aryan <aryanhooda3522@gmail.com>
2026-04-17 15:24:59 +0530

    Add brute-force solution to count occurrences of a number in an array

---
+++ src/hashing/HashingTech.java
@@ -0,0 +1,34 @@
+package hashing;
+
+// let's say array is [1,2,3,2,3,4,1]
+// let's say querry is [1,3,4,2]
+// that means we need a hash array from 0-4 that means 5 numbers
+// hash array: [0,0,0,0,0] put 0 initially in it
+// now take the querry number, go to array and check if the number is present in the array or not
+// if present go to hasharray[q[i]] and add 1 in it.
+// here q[i] is qurry index, let's say we took 1 from querry
+// checking with array 1 is present twice
+// during for loop checking add 1 in hasharray[q[i]] the number of times q[i] matches in the array
+
+import java.util.Arrays;
+
+public class HashingTech {
+//    without hashing
+//    this method takes O(Q*N) time to execute which is very large if both q and n arrays are of big sizes
+    public void findNumOccurrences(int[] arr, int num) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                count = count + 1;
+            }
+        }
+        System.out.println(count);
+    }
+//    ===========================================
+
+//    we need a simplified solution for this
+//    we can create a hasharray containing 0 value in each index
+//    as we know a array starts from index 0 and goes till the number we provide
+//    that means a hasharray of size n+1 can be created
+//    after creating hash array check array[i] with number if the array index matches the number then increase that number's point in hasharray by 1
+}
